03ea39af16488874aea164953fb8e4a3e3060e66
0xburden <0xburden@protonmail.com>
2025-09-04 23:18:41 -0700

    set up emotion stuff and remove boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+
+const StyledApp = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 1px;
+`;
+
+const StyledLayoutContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 64rem;
+  width: 100%;
+  height: 100%;
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <StyledApp className="inconsolata-variable">
+      <StyledLayoutContainer>
+        <div
+          css={css`
+            font-size: 2rem;
+            text-align: center;
+          `}
+        >
+          Hello, World!
+        </div>
+      </StyledLayoutContainer>
+    </StyledApp>
+  );
 }
 
-export default App
+export default App;
